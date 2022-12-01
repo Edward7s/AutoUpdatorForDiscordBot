bee97a4c8a3122fde0874442e3660d36b233bf18
Edward7 <ediro174@gmail.com>
2022-12-01 23:08:15 +0200

    Add project files.

---
+++ AutoUpdator/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AutoUpdator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Text = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(144, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Text
+            // 
+            this.Text.AutoSize = true;
+            this.Text.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Text.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Text.Location = new System.Drawing.Point(12, 78);
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(19, 13);
+            this.Text.TabIndex = 2;
+            this.Text.Text = "...";
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Title.Location = new System.Drawing.Point(276, 29);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(73, 18);
+            this.Title.TabIndex = 3;
+            this.Title.Text = "Updator";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label Text;
+        private Label Title;
+    }
+}
---
+++ AutoUpdator/Form1.cs
@@ -0,0 +1,167 @@
+using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace AutoUpdator
+{
+
+    public partial class Form1 : Form
+    {
+        public JObject JObject { get; set; }
+        public String[] Arguments { get; set; }
+        public string ExeDir { get; set; }
+        public bool Update { get; set; } = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Opacity = 0;
+            this.ShowInTaskbar = false;
+            if (!File.Exists(@"C:\Program Files\WinRAR\unrar.exe"))
+            {
+                this.Opacity = 1;
+                this.ShowInTaskbar = true;
+                MessageBox.Show("                < < < [ WINRAR WAS NOT FOUND. ] > > >\n" + @"< C:\Program Files\WinRAR\unrar.exe > can not be found." + "\n                                 Please Install WinRar.", "WINRAR WAS NOT FOUND.");
+                Process.GetCurrentProcess().Kill();
+            }
+            Text.Text = "-------------------------------[ Initializing Updator ]-------------------------------";
+
+            RegistryKey startup = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            ExeDir = startup.GetValue("VRCDiscordBotNotifier").ToString().Replace("VRCDiscordBotNotifier.exe", "");
+
+
+            Arguments = Environment.GetCommandLineArgs();
+            if (Arguments.Length != 2)
+            {
+                this.Opacity = 1;
+                this.ShowInTaskbar = true;
+                Text.ForeColor = Color.Red;
+                Write(" The Arguments Is Missing Please Do Not Open The Exe By Yourself.");
+                return;
+            }
+
+            string stringJson = CheckVersion("https://api.github.com/repos/Edward7s/VRCDiscordBotNotifier/releases/latest");
+            if (stringJson == string.Empty)
+            {
+                Write("Request Failed. Something Went Wrong Please Restart The App!!!");
+                Text.ForeColor = Color.Red;
+                return;
+            }
+            Write("Request Worked.");
+            JObject = JObject.Parse(stringJson);
+            Write(new StringBuilder().AppendFormat("Newest Version: {0}", JObject["tag_name"]));
+            Write(new StringBuilder().AppendFormat("Download URl: {0}", JObject["assets"][0]["browser_download_url"]));
+            Write(new StringBuilder().AppendFormat("ChangeLog: {0}", JObject["body"]));
+
+            if (Arguments[0] != JObject["tag_name"].ToString())
+            {
+                Write("Current Version Is Outdated.");
+                Update = true;
+                this.Opacity = 1;
+                this.ShowInTaskbar = true;
+            }  
+            else
+            {
+                Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+                    Process.GetCurrentProcess().Kill();
+                });
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Update) return;
+            Title.Text = "Updating In Proggress";
+            var pros = Process.GetProcessesByName("VRCDiscordBotNotifier");
+            if (pros.Length != 0)
+                for (int i = 0; i < pros.Length; i++)
+                    pros[i].Kill();
+
+            string rarPath = ExeDir + @"DiscordBot.win-x86.rar";
+            using (WebClient wc = new WebClient())
+                wc.DownloadFile(JObject["assets"][0]["browser_download_url"].ToString(), rarPath);
+            Process winRar = new Process();
+            winRar.StartInfo.FileName = @"C:\Program Files\WinRAR\unrar.exe";
+            winRar.StartInfo.CreateNoWindow = true;
+            winRar.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            winRar.EnableRaisingEvents = true;
+            winRar.StartInfo.Arguments = String.Format("x -p- {0} {1}", rarPath, ExeDir);
+            winRar.Start();
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(200);
+                    if (!Directory.Exists(ExeDir + @"win-x86")) continue;
+                    winRar.Kill();
+                    Thread.Sleep(2000);
+                    if (File.Exists(ExeDir + @"VRCDiscordBotNotifier.exe"))
+                        File.Delete(ExeDir + @"VRCDiscordBotNotifier.exe");
+                    var files = new DirectoryInfo(ExeDir + @"win-x86\").GetFiles();
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        if (!files[i].Name.EndsWith(".exe")) continue;
+                        files[i].MoveTo(ExeDir + @"\VRCDiscordBotNotifier.exe");
+                        while (true)
+                        {
+                            Thread.Sleep(200);
+                            if (!File.Exists(ExeDir + @"VRCDiscordBotNotifier.exe")) continue;
+                            Process.Start(new ProcessStartInfo() { FileName = ExeDir + @"VRCDiscordBotNotifier.exe" });
+                            break;
+                        }
+                    }
+                    break;
+                }
+            });
+            Title.ForeColor = Color.Green;
+            Title.Text = "App Will Close In 5 Seconds.";
+            Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                Process.GetCurrentProcess().Kill();
+            });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Title.Text = "App Will Close In 5 Seconds.";
+            Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                Process.GetCurrentProcess().Kill();
+            });
+        }
+        private string CheckVersion(string url)
+        {
+            Write(new StringBuilder().AppendFormat("Requesting On: {0}.", url));
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "Get";
+                request.UserAgent = "Opera";
+                request.Accept = "application/vnd.github+json";
+                request.AutomaticDecompression = DecompressionMethods.GZip;
+                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "Gzip, deflate, br");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Write(new StringBuilder().AppendFormat("Request Finished With Status: {0}.", response.StatusCode));
+                using (var streamReader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8))
+                    return streamReader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Write(ex);
+            }
+            return string.Empty;
+        }
+
+        private void Write(object obj) =>
+            Text.Text += "\n" + obj.ToString();
+
+
+    }
+}
